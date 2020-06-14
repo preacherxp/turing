9976036ed060a6c0586c4a422dcbb384dfbda9ad
preacherxp <michal.hachula@gmail.com>
2020-06-14 22:43:42 +0200

    Updates

---
+++ precache-manifest.b2c4a455ae4902c259daca331ceaf88f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06580cc1dc3e5f6b112295f3c0c1bb06",
+    "url": "/turing/index.html"
+  },
+  {
+    "revision": "beea97759af64de7e6e5",
+    "url": "/turing/static/css/main.338e85c0.chunk.css"
+  },
+  {
+    "revision": "2e0b6226428c2f86747a",
+    "url": "/turing/static/js/2.1981dda2.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/turing/static/js/2.1981dda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beea97759af64de7e6e5",
+    "url": "/turing/static/js/main.b670a47e.chunk.js"
+  },
+  {
+    "revision": "9ef2a322432dde89566c",
+    "url": "/turing/static/js/runtime-main.4c1e0dec.js"
+  }
+]);
