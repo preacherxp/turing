fda35f987c70c1cac3d865904afcaaa9af12ce4f
Michał Hachuła <michal.hachula@gmail.com>
2020-06-14 22:42:38 +0200

    Added info.

--- src/components/ExamplesSelector/index.jsx
+++ src/components/ExamplesSelector/index.jsx
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+
 import {
   numberSubstractor,
   numberAdder,
   numberAdder2,
   simpleTest,
 } from "./TuringExamples";
-import { Select, MenuItem } from "@material-ui/core";
+import { Select, MenuItem, Button } from "@material-ui/core";
+
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 450,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}))(Tooltip);
 
 export default function ExamplesSelector({
   setInstructionsTable,
@@ -21,6 +34,60 @@ export default function ExamplesSelector({
     setMemoryTape(numberSubstractor.memoryTape);
     setAlphabet(numberSubstractor.alphabet);
   }, [setAlphabet, setInstructionsTable, setMemoryTape]);
+
+  function setInfoDescription() {
+    switch (example) {
+      case "simpleTest":
+        return <React.Fragment>Simple test</React.Fragment>;
+      case "numberSubstractor":
+        return (
+          <React.Fragment>
+            A number is represented in binary format in different finite
+            automatas like 5 is represented as (101) but in case of subtraction
+            Turing Machine unary format is followed . In unary format a number
+            is represented by either all ones or all zeros. For example, 5 will
+            be represented by a sequence of five ones 5 = 1 1 1 1 1 or 0 0 0 0
+            0.
+            <br />
+            Lets use zeros for representation. For subtraction of numbers using
+            a Turing Machine, both these numbers are given as input to the
+            Turing machine separated by a “c”.
+          </React.Fragment>
+        );
+      case "numberAdder":
+        return (
+          <React.Fragment>
+            A number is represented in binary format in different finite
+            automatas like 5 is represented as (101) but in case of addition
+            using a turing machine unary format is followed. In unary format a
+            number is represented by either all ones or all zeroes.
+            <br />
+            For example, 5 will be represented by a sequence of five zeroes or
+            five ones. 5 = 1 1 1 1 1 or 0 0 0 0 0. Lets use zeroes for
+            representation. For adding 2 numbers using a Turing machine, both
+            these numbers are given as input to the Turing machine separated by
+            a “c”.
+          </React.Fragment>
+        );
+      case "numberAdder2":
+        return (
+          <React.Fragment>
+            A number is represented in binary format in different finite
+            automatas like 5 is represented as (101) but in case of addition
+            using a turing machine unary format is followed. In unary format a
+            number is represented by either all ones or all zeroes.
+            <br />
+            For example, 5 will be represented by a sequence of five zeroes or
+            five ones. 5 = 1 1 1 1 1 or 0 0 0 0 0. Lets use zeroes for
+            representation. For adding 2 numbers using a Turing machine, both
+            these numbers are given as input to the Turing machine separated by
+            a “c”.
+          </React.Fragment>
+        );
+      default:
+        break;
+    }
+  }
 
   function handleChange(e) {
     setExample(e.target.value);
@@ -57,6 +124,9 @@ export default function ExamplesSelector({
         <MenuItem value={"numberAdder"}>Number adder</MenuItem>
         <MenuItem value={"numberAdder2"}>Number adder 2</MenuItem>
       </Select>
+      <HtmlTooltip title={setInfoDescription()}>
+        <Button>Info</Button>
+      </HtmlTooltip>
     </div>
   );
 }
